516b0af27ef343b2a14d5dbd4d75299553deaae5
Kleyton Ramos <kleytonmatosramos@gmail.com>
2019-07-30 19:12:36 -0300

    Corrige testes

--- spec/requests/weapons_spec.rb
+++ spec/requests/weapons_spec.rb
@@ -18,14 +18,16 @@ RSpec.describe "Weapons", type: :request do
     it "verifica se title e o current power  estão sendo exibidos" do
       weapon = create(:weapon)
       get weapons_path
+
       expect(response.body).to have_selector('li', text: weapon.title)
       expect(response.body).to have_selector('li', text: weapon.current_power)
-      expect(response.body).to have_selector('a', text: "weapon.title")
+      expect(response.body).to have_selector('a', text: weapon.title)
     end
 
     it "verifica se o nome, current power, power base, power base, title e level são exibidos" do
       weapon = create(:weapon)
       get weapon_path(weapon)
+
       expect(response.body).to have_selector('p', text: "Name: #{weapon.name}")
       expect(response.body).to have_selector('p', text: "Power base: #{weapon.power_base}")
       expect(response.body).to have_selector('p', text: "Power step: #{weapon.power_step}")
